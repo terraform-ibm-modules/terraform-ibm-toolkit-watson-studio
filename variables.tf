2c93044c3d279bf69f3c905b0dc02dc5a1359ede
Sean Sundberg <seansund@us.ibm.com>
2022-08-24 22:47:15 -0500

    Updates plan information

--- variables.tf
+++ variables.tf
@@ -22,8 +22,8 @@ variable "name_prefix" {
 
 variable "plan" {
   type        = string
-  description = "The type of plan the service instance should run under (standard-v1, enterprise-v2)"
-  default     = "standard-v1"
+  description = "The type of plan the service instance should run under (lite, professional-v1)"
+  default     = "professional-v1"
 }
 
 variable "name" {
